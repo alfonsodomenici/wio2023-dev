940acd955c2e56d762dee136c06d3953f6f27cf5
Mario Rossi <mario.rossi@gmail.com>
2023-11-06 13:06:45 +0100

    wiodata model

--- src/app/models.py
+++ src/app/models.py
@@ -7,3 +7,11 @@ class Wio(db.Model):
     wio = db.Column(db.String(45), nullable=False, default='wio terminal')
     macaddress = db.Column(db.String(45), unique=True)
     code = db.Column(db.String(45), nullable=False)
+
+class WioData(db.Model):
+    __tablename__ = 'wiodata'
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(45), nullable=False)
+    id_wio = db.Column(db.Integer, db.ForeignKey('wio.id'), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now()) 
